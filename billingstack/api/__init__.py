50ad2eece4bb53a7a4a3a14cce857469d0fd05c6
woorea <luis@woorea.es>
2013-02-09 13:17:17 +0100

    package

---
+++ billingstack/api/__init__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2013 Woorea Solutions, S.L
+#
+# Author: Luis Gervaso <luis@woorea.es>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from billingstack.openstack.common import cfg
+
+API_SERVICE_OPTS = [
+    cfg.IntOpt('billing_api_port',
+               default=9001,
+               help='The port for the billing API server',
+               ),
+]
+
+cfg.CONF.register_opts(API_SERVICE_OPTS)
