95b2aca963119f69a145941e75c633d2602f95ba
Endre Karlson <endre.karlson@gmail.com>
2013-02-10 03:04:16 +0100

    Fix hook

--- billingstack/api/hooks.py
+++ billingstack/api/hooks.py
@@ -1,6 +1,7 @@
 from pecan import hooks
 
 from billingstack.openstack.common import cfg
+from billingstack.openstack.common import log
 from billingstack import storage
 
 
@@ -16,10 +17,10 @@ class ConfigHook(hooks.PecanHook):
 class DBHook(hooks.PecanHook):
 
     def before(self, state):
-        storage_engine = storage.get_engine(state.request.cfg)
+        storage_engine = storage.get_engine(
+            state.request.cfg['service:api'].storage_driver)
         state.request.storage_engine = storage_engine
-        state.request.storage_conn = storage_engine.get_connection(
-            state.request.cfg)
+        state.request.storage_conn = storage_engine.get_connection()
 
     # def after(self, state):
     #     print 'method:', state.request.method
