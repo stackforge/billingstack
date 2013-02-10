ffe68303c0e757f3e22be42d088a8809cd269ce6
Endre Karlson <endre.karlson@gmail.com>
2013-02-10 10:32:11 +0100

    Typos

--- billingstack/utils.py
+++ billingstack/utils.py
@@ -1,11 +1,10 @@
 import re
 
 
-def capital_to_underscored(string):
-    table_name = "_".join(l.lower() for l in re.findall('[A-Z][^A-Z]*',
-                                                        string)
-    return table_name
+def capital_to_underscore(string):
+    return "_".join(l.lower() for l in re.findall('[A-Z][^A-Z]*',
+                                                  string))
 
 
-def underscored_to_capital(word):
+def underscore_to_capital(word):
     return ''.join(x.capitalize() or '_' for x in word.split('_'))
