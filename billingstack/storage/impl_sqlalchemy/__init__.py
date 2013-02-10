6bfbc547258fd7ace23ce746378ab39c1278a26d
Endre Karlson <endre.karlson@gmail.com>
2013-02-10 02:02:56 +0100

    Base Storage API for sqla with a Merchant

--- billingstack/storage/impl_sqlalchemy/__init__.py
+++ billingstack/storage/impl_sqlalchemy/__init__.py
@@ -42,8 +42,8 @@ class Connection(base.Connection):
     """
     SQLAlchemy connection
     """
-    def __init__(self):
-        self.session = get_session()
+    def __init__(self, config_group):
+        self.session = get_session(config_group)
 
     def setup_schema(self):
         """ Semi-Private Method to create the database schema """
@@ -52,3 +52,88 @@ class Connection(base.Connection):
     def teardown_schema(self):
         """ Semi-Private Method to reset the database schema """
         models.Base.metadata.drop_all(self.session.bind)
+
+    def _save(self, obj):
+        try:
+            obj.save(self.session)
+        except exceptions.Duplicate:
+            raise
+        return obj
+
+    def _list(self, cls, criterion=None):
+        """
+        A generic list method
+
+        :param cls: The model to try to delete
+        :param criterion: Criterion to match objects with
+        """
+        query = self.session.query(cls)
+
+        if criterion:
+            query = query.filter_by(**criterion)
+
+        try:
+            result = query.all()
+        except exc.NoResultFound:
+            LOG.debug('No results found querying for %s: %s' %
+                      (cls, criterion))
+            return []
+        else:
+            return result
+
+    def _get(self, cls, id_):
+        """
+        Get an instance of a Model matching ID
+
+        :param cls: The model to try to delete
+        :param id_: The ID to delete
+        """
+        query = self.session.query(cls)
+        obj = query.get(id_)
+        if not obj:
+            raise exceptions.NotFound(id_)
+        else:
+            return obj
+
+    def _update(self, cls, id_, values):
+        """
+        Update an instance of a Model matching an ID with values
+
+        :param cls: The model to try to delete
+        :param id_: The ID to delete
+        :param values: The values to update the model instance with
+        """
+        obj = self._get(cls, id_)
+        obj.update(values)
+        try:
+            obj.save(self.session)
+        except exceptions.Duplicate:
+            raise
+        return obj
+
+    def _delete(self, cls, id_):
+        """
+        Delete an instance of a Model matching an ID
+
+        :param cls: The model to try to delete
+        :param id_: The ID to delete
+        """
+        obj = self._get(cls, id_)
+        obj.delete(self.session)
+
+    def merchant_add(self, values):
+        m = models.Merchant(**values)
+        self._save(m)
+        return dict(m)
+
+    def merchant_list(self, **kw):
+        return self._list(models.Merchant, **kw)
+
+    def merchant_get(self, merchant_id):
+        return self._get(models.Merchant, merchant_id)
+
+    def merchant_update(self, merchant_id, values):
+        return self._update(models.Merchant, merchant_id, values)
+
+    def merchant_delete(self, merchant_id):
+        self._delete(models.Merchant, merchant_id)
