96f7dd01ad44b3ee22d75e71315de93ba3919a88
Endre Karlson <endre.karlson@gmail.com>
2013-02-10 19:29:17 +0100

    Add more resources

--- billingstack/api/controllers/v1.py
+++ billingstack/api/controllers/v1.py
@@ -16,26 +16,96 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from pecan import request
 from pecan.rest import RestController
 
 import wsmeext.pecan as wsme_pecan
 from wsme.types import Base, text
 
-class Merchant(Base):
-	merchant_id = text
-	
+
+class Base(Base):
+    id = text
+
+
+class CurrencyMixin(object):
+    """
+    Mixin for a object that should show currency_id
+    """
+    currency_id = text
+
+
+class LanaguageMixin(object):
+    """
+    Mixin for a object that should show language_id
+    """
+    language_id = text
+
+
+class Currency(Base):
+    id = text
+    letter = text
+    name = text
+
+
+class Language(Base):
+    id = text
+    letter = text
+    name = text
+
+
+class User(Base):
+    pass
+
+
+class Merchant(Base, LanaguageMixin):
+    name = text
+
+
+class CurrenciesController(RestController):
+    @wsme_pecan.wsexpose([Currency], unicode)
+    def get_all(self):
+        return [Currency(**o) for o in request.storage_conn.currency_list()]
+
+
+class LanguagesController(RestController):
+    @wsme_pecan.wsexpose([Language], unicode)
+    def get_all(self):
+        return [Language(**o) for o in request.storage_conn.language_list()]
+
+
+class UsersController(RestController):
+
+    @wsme_pecan.wsexpose([User], unicode)
+    def get_all(self):
+        return [User(**o) for o in request.storage_conn.user_list()]
+
+    @wsme_pecan.wsexpose(User, unicode)
+    def get_one(self):
+        return User(*dict(id=1))
+
+
 class MerchantsController(RestController):
-	"""Merchants controller"""
-	
-	@wsme_pecan.wsexpose(Merchant, unicode)
-	def get_one(self, merchant_id):
-		"""Get merchant details
-		
-		:param merchant_id: The UUID of the merchant
-		"""
-		return Merchant()
-		
+    """Merchants controller"""
+
+    users = UsersController()
+
+    @wsme_pecan.wsexpose([Merchant], unicode)
+    def get_all(self):
+        return [Merchant(**o) for o in request.storage_conn.merchant_list()]
+
+    @wsme_pecan.wsexpose(Merchant, unicode)
+    def get_one(self, merchant_id):
+        """Get merchant details
+
+        :param merchant_id: The UUID of the merchant
+        """
+        m = request.storage_conn.merchant_get(merchant_id)
+        return Merchant(**dict(m))
+
+
 class V1Controller(object):
-	"""Version 1 API controller."""
-	
-	merchants = MerchantsController()
+    """Version 1 API controller."""
+
+    currencies = CurrenciesController()
+    languages = LanguagesController()
+    merchants = MerchantsController()
--- billingstack/storage/impl_sqlalchemy/__init__.py
+++ billingstack/storage/impl_sqlalchemy/__init__.py
@@ -18,6 +18,7 @@ from sqlalchemy.orm import exc
 from billingstack.openstack.common import cfg
 from billingstack.openstack.common import log as logging
 from billingstack import exceptions
+from billingstack import utils
 from billingstack.storage import base
 from billingstack.storage.impl_sqlalchemy import models
 from billingstack.storage.impl_sqlalchemy.session import get_session, SQLOPTS
@@ -47,11 +48,11 @@ class Connection(base.Connection):
 
     def setup_schema(self):
         """ Semi-Private Method to create the database schema """
-        models.Base.metadata.create_all(self.session.bind)
+        models.ModelBase.metadata.create_all(self.session.bind)
 
     def teardown_schema(self):
         """ Semi-Private Method to reset the database schema """
-        models.Base.metadata.drop_all(self.session.bind)
+        models.ModelBase.metadata.drop_all(self.session.bind)
 
     def _save(self, obj):
         try:
@@ -121,19 +122,77 @@ class Connection(base.Connection):
         obj = self._get(cls, id_)
         obj.delete(self.session)
 
+    def _serialize(self, data):
+        if isinstance(data, list):
+            return [dict(o) for o in data]
+        else:
+            return dict(data)
+
+    # Currency
+    def currency_add(self, values):
+        """
+        Add a supported currency to the database
+        """
+        currency = utils.get_currency(values['letter'])
+        obj = self._save(models.Currency(**currency))
+        return self._serialize(obj)
+
+    def currency_list(self):
+        rows = self._list(models.Currency)
+        return self._serialize(rows)
+
+    def currency_get(self, currency_id):
+        obj = self._get(models.Currency, currency_id)
+        return self._serialize(obj)
+
+    def currency_update(self, currency_id, values):
+        obj = self._update(models.Currency, currency_id, values)
+        return self._serialize(obj)
+
+    def currency_delete(self, currency_id):
+        obj = self._get(models.Currency)
+        obj.delete()
+
+    # Language
+    def language_add(self, values):
+        """
+        Add a supported language to the database
+        """
+        data = utils.get_language(values['letter'])
+        obj = self._save(models.Language(**data))
+        return self._serialize(obj)
+
+    def language_list(self):
+        rows = self._list(models.Language)
+        return self._serialize(rows)
+
+    def language_get(self, id_):
+        obj = self._get(models.Currency, id_)
+        return self._serialize(obj)
+
+    def language_update(self, id_, values):
+        obj = self._update(models.Language, id_, values)
+        return self._serialize(obj)
+
+    def language_delete(self, id_):
+        obj = self._get(models.Language)
+        obj.delete()
+
+    # Merchant
     def merchant_add(self, values):
-        m = models.Merchant(**values)
-        self._save(m)
-        return dict(m)
+        return dict(self._save(models.Merchant(**values)))
 
     def merchant_list(self, **kw):
-        return self._list(models.Merchant, **kw)
+        return [dict(o) for o in self._list(models.Merchant, **kw)]
 
     def merchant_get(self, merchant_id):
-        return self._get(models.Merchant, merchant_id)
+        return dict(self._get(models.Merchant, merchant_id))
 
     def merchant_update(self, merchant_id, values):
-        return self._update(models.Merchant, merchant_id, values)
+        return dict(self._update(models.Merchant, merchant_id, values))
 
     def merchant_delete(self, merchant_id):
         self._delete(models.Merchant, merchant_id)
+
+    def user_list(self, **kw):
+        return [dict(o) for o in self._list(models.User, **kw)]
