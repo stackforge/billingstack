96d8fcdedac805c8f449ae5e83b958b2cea5c859
Endre Karlson <endre.karlson@gmail.com>
2013-02-13 19:54:02 +0100

    Base TestCase that uses samples

---
+++ billingstack/tests/__init__.py
@@ -0,0 +1,80 @@
+import copy
+import unittest2
+import mox
+from billingstack.openstack.common import cfg
+# NOTE: Currently disabled
+# from billingstack.openstack.common import policy
+from billingstack.openstack.common import log as logging
+from billingstack import samples
+from billingstack import storage
+from billingstack import exceptions
+
+
+class AssertMixin(object):
+    """
+    Mixin to hold assert helpers.
+
+    """
+    def assertLen(self, expected_length, obj):
+        """
+        Assert a length of a object
+
+        :param obj: The object ot run len() on
+        :param expected_length: The length in Int that's expected from len(obj)
+        """
+        self.assertEqual(len(obj), expected_length)
+
+    def assertData(self, expected_data, data):
+        """
+        A simple helper to very that at least fixture data is the same
+        as returned
+
+        :param expected_data: Data that's expected
+        :param data: Data to check expected_data against
+        """
+        for key, value in expected_data.items():
+            self.assertEqual(data[key], value)
+
+
+class TestCase(unittest2.TestCase, AssertMixin):
+    def setUp(self):
+        super(TestCase, self).setUp()
+
+        self.mox = mox.Mox()
+
+        self.config(
+            storage_driver='sqlalchemy',
+            group='service:api'
+        )
+
+        self.config(
+            database_connection='sqlite://',
+            group='storage:sqlalchemy'
+        )
+
+        self.samples = samples.get_samples()
+
+        storage.setup_schema()
+
+    def tearDown(self):
+        # NOTE: Currently disabled
+        #policy.reset()
+        storage.teardown_schema()
+        cfg.CONF.reset()
+        self.mox.UnsetStubs()
+        super(TestCase, self).tearDown()
+
+    # Config Methods
+    def config(self, **kwargs):
+        group = kwargs.pop('group', None)
+
+        for k, v in kwargs.iteritems():
+            cfg.CONF.set_override(k, v, group)
+
+    def get_fixture(self, name, fixture=0, values={}):
+        """
+        Get a fixture from self.samples and override values if necassary
+        """
+        _values = copy.copy(self.samples.get[name][fixture])
+        _values.update(values)
+        return _values
