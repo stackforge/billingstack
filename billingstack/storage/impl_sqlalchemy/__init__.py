527f4c5dc773dd057e838be59f0d4bfe300b6f71
Endre Karlson <endre.karlson@gmail.com>
2013-02-14 10:44:08 +0100

    Fix license header

--- billingstack/storage/impl_sqlalchemy/__init__.py
+++ billingstack/storage/impl_sqlalchemy/__init__.py
@@ -1,6 +1,4 @@
-# Copyright 2012 Managed I.T.
-#
-# Author: Kiall Mac Innes <kiall@managedit.ie>
+# Author: Endre Karlson <endre.karlson@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
--- billingstack/storage/impl_sqlalchemy/models.py
+++ billingstack/storage/impl_sqlalchemy/models.py
@@ -1,5 +1,3 @@
-# Copyright 2012 Bouvet ASA
-#
 # Author: Endre Karlson <endre.karlson@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
