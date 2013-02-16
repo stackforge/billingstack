993c26244f02c0a7c59bc79fe2ab18ed93588aae
Endre Karlson <endre.karlson@gmail.com>
2013-02-16 17:36:17 +0100

    Unused import

--- billingstack/storage/impl_sqlalchemy/__init__.py
+++ billingstack/storage/impl_sqlalchemy/__init__.py
@@ -11,7 +11,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import time
 from sqlalchemy.orm import exc
 from billingstack.openstack.common import cfg
 from billingstack.openstack.common import log as logging
@@ -20,7 +19,6 @@ from billingstack import utils as cutils
 from billingstack.storage import base
 from billingstack.storage.impl_sqlalchemy import models
 from billingstack.storage.impl_sqlalchemy.session import get_session, SQLOPTS
-from billingstack.storage.impl_sqlalchemy import utils as db_utils
 
 LOG = logging.getLogger(__name__)
 
