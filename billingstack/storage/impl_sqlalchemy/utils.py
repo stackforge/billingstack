72c5ec7921df485f45077e2a0a24572bbbc18e2d
Endre Karlson <endre.karlson@gmail.com>
2013-02-24 14:32:24 +0100

    Not needed, same function is in get_prop_names

--- billingstack/storage/impl_sqlalchemy/utils.py
+++ billingstack/storage/impl_sqlalchemy/utils.py
@@ -10,23 +10,3 @@ def get_prop_names(obj, exclude=[]):
             if isinstance(p, RelationshipProperty):
                 remote.append(p.key)
     return local, remote
-
-
-def get_local_values(model, values):
-    """
-    Make a new dictionary out of values containing only values relevant to
-    the Model
-
-    Example usage:
-        values, metadata = get_local_values(models.Product, values)
-
-    :param model: SQLAlchemy Model
-    :param values: Values to get from
-    """
-    local, _ = get_prop_names(model)
-
-    new_values = {}
-    for key in values.keys():
-        if key in local:
-            new_values[key] = values.pop(key)
-    return new_values, values
