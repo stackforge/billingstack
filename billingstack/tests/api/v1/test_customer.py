2d6315af83c3aad269ce36b1aa0fabffdb4400ac
Endre Karlson <endre.karlson@gmail.com>
2013-03-10 13:35:57 +0000

    Fix doc

--- billingstack/tests/api/v1/test_customer.py
+++ billingstack/tests/api/v1/test_customer.py
@@ -13,8 +13,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-#
-"""Test listing raw events.
+"""
+Test Customers.
 """
 
 from billingstack.tests.api.v1.base import FunctionalTest
