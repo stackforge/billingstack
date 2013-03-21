a7204ef7fe1ce22b92c9d54c468e235a4a99f59a
Endre Karlson <endre.karlson@gmail.com>
2013-03-21 21:35:43 +0000

    First step to adding more checks to code

--- billingstack/central/service.py
+++ billingstack/central/service.py
@@ -43,3 +43,221 @@ class Service(rpc_service.Service):
             return f(*args, **kw)
         setattr(self, name, _wrapper)
         return _wrapper
+
+    def create_currency(self, ctxt, values):
+        return self.create_currency(ctxt, values)
+
+    def list_currencies(self, ctxt, **kw):
+        return self.storage_conn.list_currencies(ctxt, **kw)
+
+    def get_currency(self, ctxt, id_):
+        return self.storage.get_currency(ctxt, id_)
+
+    def update_currency(self, ctxt, id_, values):
+        return self.storage_conn.update_currency(ctxt, id_, values)
+
+    def delete_currency(self, ctxt, id_):
+        return self.storage_conn.delete_currency(ctxt, id_)
+
+    def create_language(self, ctxt, values):
+        return self.storage_conn.create_language(ctxt, values)
+
+    def list_languages(self, ctxt, **kw):
+        return self.storage_conn.list_languages(ctxt, **kw)
+
+    def get_language(self, ctxt, id_):
+        return self.storage_conn.get_language(ctxt, id_)
+
+    def update_language(self, ctxt, id_, values):
+        return self.storage_conn.update_language(ctxt, id_, values)
+
+    def delete_language(self, ctxt, id_):
+        return self.storage_conn.delete_language(ctxt, id_)
+
+    def create_invoice_state(self, ctxt, values):
+        return self.storage_conn.create_invoice_state(ctxt, values)
+
+    def list_invoice_states(self, ctxt, **kw):
+        return self.storage_conn.list_invoice_states(ctxt, **kw)
+
+    def get_invoice_state(self, ctxt, id_):
+        return self.storage_conn.get_invoice_state(ctxt, id_)
+
+    def update_invoice_state(self, ctxt, id_, values):
+        return self.storage_conn.update_invoice_state(ctxt, id_)
+
+    def delete_invoice_state(self, ctxt, id_):
+        return self.storage_conn.delete_invoice_state(ctxt, id_)
+
+    # TODO Fix
+    def create_contact_info(self, ctxt, obj, values, cls=None,
+                            rel_attr='contact_info'):
+        return self.storage_conn.create_contact_info(ctxt, values)
+
+    def get_contact_info(self, ctxt, id_):
+        return self.storage_conn.get_contact_info(ctxt, id_)
+
+    def update_contact_info(self, ctxt, id_, values):
+        return self.storage_conn.update_contact_info(ctxt, values)
+
+    def delete_contact_info(self, ctxt, id_):
+        return self.storage_conn.delete_contact_info(ctxt, id_)
+
+    def list_pg_providers(self, ctxt, **kw):
+        return self.storage_conn.list_pg_providers(ctxt, **kw)
+
+    def get_pg_provider(self, ctxt, pgp_id):
+        return self.storage_conn.get_pg_provider(ctxt, pgp_id)
+
+    def create_pg_method(self, ctxt, values):
+        return self.storage_conn.create_pg_method(ctxt, values)
+
+    def list_pg_methods(self, ctxt, **kw):
+        return self.storage_conn.list_pg_methods(ctxt, **kw)
+
+    def get_pg_method(self, ctxt, id_):
+        return self.storage_conn.get_pg_method(ctxt, id_)
+
+    def update_pg_method(self, ctxt, id_, values):
+        return self.storage_conn.update_pg_method(ctxt, id_, values)
+
+    def delete_pg_method(self, ctxt, id_):
+        return self.storage_conn.delete_pg_method(ctxt, id_)
+
+    def create_pg_config(self, ctxt, merchant_id, provider_id, values):
+        return self.storage_conn.create_pg_config(ctxt, merchant_id,
+                                                  provider_id, values)
+
+    def list_pg_configs(self, ctxt, **kw):
+        return self.storage_conn.list_pg_configs(ctxt, **kw)
+
+    def get_pg_config(self, ctxt, id_):
+        return self.storage_conn.get_pg_config(ctxt, id_)
+
+    def update_pg_config(self, ctxt, id_, values):
+        return self.storage_conn.update_pg_config(ctxt, id_, values)
+
+    def delete_pg_config(self, ctxt, id_):
+        return self.storage_conn.delete_pg_config(ctxt, id_)
+
+    def create_payment_method(self, ctxt, customer_id, pg_method_id, values):
+        return self.storage_conn.create_payment_method(
+            ctxt, customer_id, pg_method_id, values)
+
+    def list_payment_methods(self, ctxt, **kw):
+        return self.storage_conn.list_payment_methods(ctxt, **kw)
+
+    def get_payment_method(self, ctxt, id_, **kw):
+        return self.storage_conn.get_payment_method(ctxt, id_)
+
+    def update_payment_method(self, ctxt, id_, values):
+        return self.storage_conn.update_payment_method(ctxt, id_, values)
+
+    def delete_payment_method(self, ctxt, id_):
+        return self.storage_conn.delete_payment_method(ctxt, id_)
+
+    def create_merchant(self, ctxt, values):
+        return self.storage_conn.create_merchant(ctxt, values)
+
+    def list_merchants(self, ctxt, **kw):
+        return self.storage_conn.list_merchants(ctxt, **kw)
+
+    def get_merchant(self, ctxt, id_):
+        return self.storage_conn.get_merchant(ctxt, id_)
+
+    def update_merchant(self, ctxt, id_, values):
+        return self.storage_conn.update_merchant(ctxt, id_, values)
+
+    def delete_merchant(self, ctxt, id_):
+        return self.storage_conn.delete_merchant(ctxt, id_)
+
+    def create_customer(self, ctxt, merchant_id, values):
+        return self.storage_conn.create_customer(ctxt, merchant_id, values)
+
+    def list_customers(self, ctxt, **kw):
+        return self.storage_conn.list_customers(ctxt, **kw)
+
+    def get_customer(self, ctxt, id_):
+        return self.storage_conn.get_customer(ctxt, id_)
+
+    def update_customer(self, ctxt, id_, values):
+        return self.storage_conn.update_customer(ctxt, id_, values)
+
+    def delete_customer(self, ctxt, id_):
+        return self.storage_conn.delete_customer(ctxt, id_)
+
+    def create_plan(self, ctxt, merchant_id, values):
+        return self.storage_conn.create_plan(ctxt, merchant_id, values)
+
+    def list_plans(self, ctxt, **kw):
+        return self.storage_conn.list_plans(ctxt, **kw)
+
+    def get_plan(self, ctxt, id_):
+        return self.storage_conn.get_plan(ctxt, id_)
+
+    def update_plan(self, ctxt, id_, values):
+        return self.storage_conn.update_plan(ctxt, id_, values)
+
+    def delete_plan(self, ctxt, id_):
+        return self.storage_conn.delete_plan(ctxt, id_)
+
+    def create_plan_item(self, ctxt, values):
+        return self.storage_conn.create_plan(ctxt, values)
+
+    def update_plan_item(self, ctxt, id_, values):
+        return self.storage_conn.update_plan_item(ctxt, id_, values)
+
+    def list_plan_items(self, ctxt, **kw):
+        return self.storage_conn.list_plan_items(ctxt, **kw)
+
+    def get_plan_item(self, ctxt, id_):
+        return self.storage_conn.get_plan_item(ctxt, id_)
+
+    def delete_plan_item(self, ctxt, id_):
+        return self.storage_conn.delete_plan_item(ctxt, id_)
+
+    def create_product(self, ctxt, merchant_id, values):
+        return self.storage_conn.create_product(ctxt, merchant_id, values)
+
+    def list_products(self, ctxt, **kw):
+        return self.storage_conn.list_products(ctxt, **kw)
+
+    def get_product(self, ctxt, id_):
+        return self.storage_conn.get_product(ctxt, id_)
+
+    def update_product(self, ctxt, id_, values):
+        return self.storage_conn.update_product(ctxt, id_, values)
+
+    def delete_product(self, ctxt, id_):
+        return self.storage_conn.delete_product(ctxt, id_)
+
+    def create_invoice(self, ctxt, merchant_id, values):
+        return self.storage_conn.create_invoice_state(
+            ctxt, merchant_id, values)
+
+    def list_invoices(self, ctxt, **kw):
+        return self.storage_conn.list_invoices(ctxt, **kw)
+
+    def get_invoice(self, ctxt, id_):
+        return self.storage_conn.get_invoice(ctxt, id_)
+
+    def update_invoice(self, ctxt, id_, values):
+        return self.storage_conn.update_invoice(ctxt, id_, values)
+
+    def delete_invoice(self, ctxt, id_):
+        return self.storage_conn.delete_invoice(ctxt, id_)
+
+    def create_subscription(self, ctxt, customer_id, values):
+        return self.storage_conn.create_subscription(ctxt, customer_id, values)
+
+    def list_subscriptions(self, ctxt, **kw):
+        return self.storage_conn.list_subscriptions(ctxt, **kw)
+
+    def get_subscription(self, ctxt, id_):
+        return self.storage_conn.get_subscription(ctxt, id_)
+
+    def update_subscription(self, ctxt, id_, values):
+        return self.storage_conn.update_subscription(ctxt, id_, values)
+
+    def delete_subscription(self, ctxt, id_):
+        return self.storage_conn.delete_subscription(ctxt, id_)
