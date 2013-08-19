d058b242dce6b34186412cfc16206f9d204aa222
Endre Karlson <endre.karlson@gmail.com>
2013-08-19 21:12:32 +0200

    Update common stuffs
    Change-Id: Id943dafb19638488f025362ece7ababc5d79219e

--- billingstack/central/__init__.py
+++ billingstack/central/__init__.py
@@ -19,6 +19,7 @@ cfg.CONF.register_group(cfg.OptGroup(
     name='service:central', title="Configuration for Central Service"
 ))
 
+
 cfg.CONF.register_opts([
     cfg.IntOpt('workers', default=None,
                help='Number of worker processes to spawn'),
